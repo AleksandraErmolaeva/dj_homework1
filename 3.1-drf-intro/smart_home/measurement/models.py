11ae263a978d7a965d0d0c2ac9a0faba5dadc1b2
Aleksandra <alexandraishanina@gmail.com>
2023-12-06 18:24:43 +0300

    task3.1 done

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+class Sensor(models.Model):
+    name = models.CharField(max_length = 20, unique = True, verbose_name = 'Датчик')
+    description = models.CharField(max_length = 100)
+
+    class Meta:
+        verbose_name = 'Датчик'
+        verbose_name_plural = 'Датчики'
+
+    def __str__(self):
+        return self.name
+
+class Measurement(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete = models.CASCADE, related_name = 'measurements')
+    temperature = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f' {self.created_at}, {self.sensor}, {self.temperature}'
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,17 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from measurement.models import Sensor, Measurement
+from .serializers import MeasurementSerializer, SensorSerializer, SensorMeasurementSerializer
+
+class SensorsView(ListCreateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+class SensorView(RetrieveUpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorMeasurementSerializer
+
+class MeasurementView(CreateAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
